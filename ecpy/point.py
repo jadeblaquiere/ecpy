6a20d1bf8516d53b61ad79dfb30ce408cb14877c
Joe deBlaquiere <jadb@Mac-Mini.local>
2016-03-26 00:54:08 -0400

    initial import

---
+++ ecpy/point.py
@@ -0,0 +1,220 @@
+# Copyright (c) 2016, Joseph deBlaquiere <jadeblaquiere@yahoo.com>
+# All rights reserved
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of controller-example nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import curves
+
+_curve = curves.curve_secp256k1
+# _curve = curves.curve_secp384r1
+# _curve = curves.curve_bauer9
+
+_generator_LUT_bits = 8
+
+
+def _modinv(a, m):
+    lastr, r, x, lastx = a, m, 0, 1
+    while r:
+        lastr, (q, r) = r, divmod(lastr, r)
+        x, lastx = lastx - q*x, x
+    return lastx % m
+
+
+class Point (object):
+    p = _curve['p']
+    n = _curve['n']
+    a = _curve['a']
+    bits = _curve['bits']
+
+    def __init__(self, x=None, y=None, z=None, infinity=False):
+        self.x = x
+        self.y = y
+        self.z = z if z is not None else 1
+        self.is_infinite = True if x is None else infinity
+        self.dcache = None
+
+    @classmethod
+    def set_curve(cls, c):
+        cls.p = c['p']
+        cls.n = c['n']
+        cls.a = c['a']
+        cls.bits = c['bits']
+
+    def _from_jacobian(self):
+        if not self.is_infinite:
+            zinv = _modinv(self.z, Point.p)
+            self.x = (self.x * zinv ** 2) % Point.p
+            self.y = (self.y * zinv ** 3) % Point.p
+            self.z = 1
+
+    def _copy(self):
+        return Point(self.x, self.y, self.z, self.is_infinite)
+
+    def _double(self):
+        if self.is_infinite or self.y == 0:
+            return Point(infinity=True)
+        ysq = (self.y * self.y) % Point.p
+        S = (4 * self.x * ysq) % Point.p
+        M = (3 * self.x * self.x + Point.a * self.z ** 4) % Point.p
+        nx = (M * M - 2 * S) % self.p
+        ny = (M * (S - nx) - 8 * ysq * ysq) % Point.p
+        nz = (2 * self.y * self.z) % Point.p
+        return Point(nx, ny, nz)
+
+    def __eq__(self, q):
+        if self.is_infinite:
+            if q.is_infinite:
+                return True
+            else:
+                return False
+        U1 = (self.x * q.z * q.z) % Point.p
+        U2 = (q.x * self.z * self.z) % Point.p
+        S1 = (self.y * q.z ** 3) % Point.p
+        S2 = (q.y * self.z ** 3) % Point.p
+        if U1 == U2:
+            if S1 == S2:
+                return True
+        return False
+
+    def __ne__(self, q):
+        return not (self == q)
+
+    def __add__(self, q):
+        if self.is_infinite:
+            return q._copy()
+        if q.is_infinite:
+            return self._copy()
+        U1 = (self.x * q.z * q.z) % Point.p
+        U2 = (q.x * self.z * self.z) % Point.p
+        S1 = (self.y * q.z ** 3) % Point.p
+        S2 = (q.y * self.z ** 3) % Point.p
+        if U1 == U2:
+            if S1 != S2:
+                return Point(infinity=True)
+            return self._double()
+        H = U2 - U1
+        R = S2 - S1
+        H2 = (H * H) % Point.p
+        H3 = (H * H2) % Point.p
+        U1H2 = (U1 * H2) % Point.p
+        nx = (R * R - H3 - 2 * U1H2) % Point.p
+        ny = (R * (U1H2 - nx) - S1 * H3) % Point.p
+        nz = (H * self.z * q.z) % Point.p
+        return Point(nx, ny, nz)
+
+    def _base_multiply(self, n):
+        if self.is_infinite or n == 0:
+            return Point(infinity=True)
+        if n == 1:
+            return self._copy()
+        nmod = n % Point.n
+        b = self.multiply(nmod//2)
+        if (nmod % 2) == 0:
+            return b.double()
+        return self.add(b.double())
+
+    def __mul__(self, n):
+        if self.is_infinite or n == 0:
+            return Point(infinity=True)
+        nmod = n % Point.n
+        if self.dcache is None:
+            d = self._copy()
+            self.dcache = []
+            accum = Point(infinity=True)
+            cn = self.bits
+            while cn > 0:
+                dval = (d.x, d.y, d.z, d.is_infinite)
+                self.dcache.append(dval)
+                if (nmod % 2) == 1:
+                    accum += (d)
+                if cn == 1:
+                    return accum
+                d = d._double()
+                nmod = nmod // 2
+                cn -= 1
+        else:
+            accum = Point(infinity=True)
+            idx = 0
+            while nmod > 0:
+                if (nmod % 2) == 1:
+                    dval = self.dcache[idx]
+                    accum += Point(dval[0], dval[1], dval[2], dval[3])
+                if nmod == 1:
+                    return accum
+                nmod = nmod // 2
+                idx += 1
+
+    def __rmul__(self, n):
+        return __mul__(self, n)
+
+    def affine(self):
+        self._from_jacobian()
+        if self.is_infinite:
+            return 'infinity'
+        return (self.x, self.y)
+
+
+class Generator (Point):
+
+    def __init__(self, x, y, z=None, infinity=False):
+        super(self.__class__, self).__init__(x, y)
+        self._recalculate_tables()
+
+    def _recalculate_tables(self):
+        self.ntables = ((Generator.bits - 1) // _generator_LUT_bits) + 1
+        self.tablesize = 2 ** _generator_LUT_bits
+        self.maskbits = self.tablesize - 1
+        super(self.__class__, self).__mul__(1)
+        bitbase = 0
+        self.lut = []
+        while bitbase < Generator.bits:
+            nlut = []
+            for i in range(self.tablesize):
+                accum = Point(infinity=True)
+                for j in range(_generator_LUT_bits):
+                    if bitbase+j < Generator.bits:
+                        if (i & (0x01 << j)) != 0:
+                            dval = self.dcache[bitbase + j]
+                            accum = accum + Point(
+                                dval[0], dval[1], dval[2], dval[3])
+                nlut.append(accum)
+            accum._from_jacobian()
+            self.lut.append(nlut)
+            bitbase += _generator_LUT_bits
+
+    def __mul__(self, n):
+        nshift = n % Generator.n
+        iidx = 0
+        accum = Point(infinity=True)
+        while nshift != 0:
+            idx = nshift & self.maskbits
+            accum = accum + self.lut[iidx][idx]
+            iidx += 1
+            nshift >>= _generator_LUT_bits
+        return accum
+
+    def __rmul__(self, n):
+        return __mul__(self, n)
---
+++ tests/test_point.py
@@ -0,0 +1,339 @@
+# Copyright (c) 2016, Joseph deBlaquiere <jadeblaquiere@yahoo.com>
+# All rights reserved
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of controller-example nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from ecpy import Point, Generator
+from ecpy import curves
+import time
+import random
+
+_curve = curves.curve_secp256k1
+#_curve = curves.curve_secp384r1
+#_curve = curves.curve_bauer9
+P = _curve['p']
+N = _curve['n']
+A = _curve['a']
+
+Point.set_curve(_curve)
+
+def modinv(a, m): 
+    lastremainder, remainder, x, lastx = a, m, 0, 1
+    while remainder:
+        lastremainder, (quotient, remainder) = remainder, divmod(lastremainder, remainder)
+        x, lastx = lastx - quotient*x, x
+    return lastx % m
+
+def inv(a, n):
+    if a == 0:
+        return 0
+    lm, hm = 1, 0
+    low, high = a % n, n
+    while low > 1:
+        r = high // low
+        nm, new = hm - lm * r, high - low * r
+        lm, low, hm, high = nm, new, lm, low
+    return lm % n
+
+#def inv(a,n):
+#    return pow(a,n-2,n)
+
+
+
+def isinf(p):
+    return p[0] == 0 and p[1] == 0
+
+
+def to_jacobian(p):
+    o = (p[0], p[1], 1)
+    return o
+
+
+def jacobian_double(p):
+    if not p[1]:
+        return (0, 0, 0)
+    ysq = (p[1] ** 2) % P
+    S = (4 * p[0] * ysq) % P
+    M = (3 * p[0] ** 2 + A * p[2] ** 4) % P
+    nx = (M**2 - 2 * S) % P
+    ny = (M * (S - nx) - 8 * ysq ** 2) % P
+    nz = (2 * p[1] * p[2]) % P
+    return (nx, ny, nz)
+
+
+def jacobian_add(p, q):
+    if not p[1] and not p[0]:
+        return q
+    if not q[1] and not q[0]:
+        return p
+    #print 'p', str(p)
+    #print 'q', str(q)
+    U1 = (p[0] * q[2] ** 2) % P
+    U2 = (q[0] * p[2] ** 2) % P
+    S1 = (p[1] * q[2] ** 3) % P
+    S2 = (q[1] * p[2] ** 3) % P
+    #print 't', U1, U2, S1, S2
+    if U1 == U2:
+        if S1 != S2:
+            return (0, 0, 1)
+        return jacobian_double(p)
+    H = U2 - U1
+    R = S2 - S1
+    H2 = (H * H) % P
+    H3 = (H * H2) % P
+    U1H2 = (U1 * H2) % P
+    #print 'b', H, R, H2, H3, U1H2
+    nx = (R ** 2 - H3 - 2 * U1H2) % P
+    ny = (R * (U1H2 - nx) - S1 * H3) % P
+    nz = (H * p[2] * q[2]) % P
+    return (nx, ny, nz)
+
+
+def from_jacobian(p):
+    z = inv(p[2], P)
+    return ((p[0] * z**2) % P, (p[1] * z**3) % P)
+
+
+def jacobian_multiply(a, n):
+    if a[1] == 0 or n == 0:
+        return (0, 0, 1)
+    if n == 1:
+        return a
+    if n < 0 or n >= N:
+        return jacobian_multiply(a, n % N)
+    if (n % 2) == 0:
+        return jacobian_double(jacobian_multiply(a, n//2))
+    if (n % 2) == 1:
+        return jacobian_add(jacobian_double(jacobian_multiply(a, n//2)), a)
+
+
+def fast_multiply(a, n):
+    return from_jacobian(jacobian_multiply(to_jacobian(a), n))
+
+
+def fast_add(a, b):
+    return from_jacobian(jacobian_add(to_jacobian(a), to_jacobian(b)))
+
+if __name__ == '__main__':
+    p = _curve['p']
+    if False:
+        for x in range(min(512,p)):
+            xi = inv(x, p)
+            test = (x*xi) % p
+            #print x, xi, x*xi, test
+            if x != 0:
+                assert test == 1
+    
+    print 'Setting up Generators'
+    G = _curve['G']
+    print 'ref Gen'
+    Gpt = Point(G[0],G[1])
+    print 'Point Gen'
+    GenG = Generator(G[0],G[1])
+    print 'Generator Gen'
+        
+    if True:
+        limit = min(512, _curve['n']) / 2 
+        for ax in range(1-limit,limit+1):
+            print 'ax=', ax
+            x = ax % _curve['n']
+            for ay in range(1-limit,limit+1):
+                y = ay % _curve['n']
+                X = fast_multiply(G, x)
+                Xpt = Point(X[0], X[1]) if x != 0 else Point(infinity=True)
+                Xa = Xpt.affine()
+                Y = fast_multiply(G, y)
+                Ypt = Point(Y[0], Y[1]) if y != 0 else Point(infinity=True)
+                Ya = Ypt.affine()
+                 #print 'fd'
+                XX = fast_add(X, X)
+                XXpt = Xpt._double()
+                XXa = XXpt.affine()
+                #print 'fa'
+                XY = fast_add(X, Y)
+                XYpt = Xpt + Ypt
+                XYa = XYpt.affine()
+                #print str(x), str(y), str(Xa), str(GXa), str(X), str(Ya), str(GYa), str(Y), str(XX), str(XXa), str(GXXa), str(XY), str(XYa), str(GXYa)
+                if XXpt.is_infinite:
+                    assert XX[0] == 0 and XX[1] == 0
+                else:
+                    assert XXa[0] == XX[0] and XXa[1] == XX[1]
+                if XYpt.is_infinite:
+                    assert XY[0] == 0 and XY[1] == 0
+                else:
+                    assert XYa[0] == XY[0] and XYa[1] == XY[1]
+                xy = (x+y) % _curve['n']
+                Fxy = fast_multiply(G,xy)
+                Pxy = Gpt * xy
+                Pxya = Pxy.affine()
+                #print str(xy) + ':', _curve['n'], Fxy, Pxya, GPxya
+                if Pxy.is_infinite:
+                    assert Fxy[0] == 0 and Fxy[1] == 0
+                    assert XYpt.is_infinite
+                else:
+                    assert Pxya[0] == Fxy[0] and Pxya[1] == Fxy[1]
+                    assert Pxya[0] == XYa[0] and Pxya[1] == XYa[1]
+    
+    
+    testset = []
+    mpztestset = []
+    for x in range(250):
+        r = random.randint(0, _curve['n'] - 1)
+        testset.append(r)
+    
+    tval = []
+    for x in testset:
+        tval.append(fast_multiply(G,x))
+    brpd_time = time.time()
+    for X in tval:
+        for y in range(100):
+            Pdoub = fast_add(X, X)
+    arpd_time = time.time()
+    
+    tval = []
+    for x in testset:
+        Gx = Gpt * x
+        #Gx._from_jacobian()
+        tval.append(Gx)
+    bpd_time = time.time()
+    for X in tval:
+        for y in range(100):
+            Gdoub = X._double()
+            #Gda = Gdoub.affine()
+    apd_time = time.time()
+    
+    ref_time = arpd_time - brpd_time
+    orig_time = apd_time - bpd_time
+    
+    print 'doubling:'
+    print 'ref =', ref_time
+    print 'ecp =', orig_time
+    
+    testset = []
+    mpztestset = []
+    for x in range(250):
+        r = random.randint(0, _curve['n'] - 1)
+        s = random.randint(0, _curve['n'] - 1)
+        testset.append((r, s))
+    
+    tval = []
+    for x in testset:
+        tval.append((fast_multiply(G,x[0]), fast_multiply(G,x[1])))
+    brpd_time = time.time()
+    for X in tval:
+        for y in range(100):
+            Padd = fast_add(X[0], X[1])
+    arpd_time = time.time()
+    
+    tval = []
+    for x in testset:
+        Gx0 = Gpt * x[0]
+        #Gx0._from_jacobian()
+        Gx1 = Gpt * x[1]
+        #Gx1._from_jacobian()
+        tval.append((Gx0, Gx1))
+    bpd_time = time.time()
+    for X in tval:
+        for y in range(100):
+            Gadd = X[0] + X[1]
+            #Gaa = Gadd.affine()
+    apd_time = time.time()
+    
+    ref_time = arpd_time - brpd_time
+    orig_time = apd_time - bpd_time
+    
+    print 'addition'
+    print 'ref =', ref_time
+    print 'ecp =', orig_time
+
+    testset = []
+    mpztestset = []
+    for x in range(25):
+        r = random.randint(0, _curve['n'] - 1)
+        s = random.randint(0, _curve['n'] - 1)
+        testset.append((r, s))
+    
+    tval = []
+    brpd_time = time.time()
+    for x in testset:
+        X = fast_multiply(G,x[0])
+        for y in range(100):
+            Pmul = fast_multiply(X, x[1])
+    arpd_time = time.time()
+    
+    tval = []
+    bpd_time = time.time()
+    for x in testset:
+        X = Gpt * x[0]
+        #X._from_jacobian()
+        for y in range(100):
+            Gmul = X * x[1]
+            #Gmula = Gmul.affine()
+    apd_time = time.time()
+    
+    ref_time = arpd_time - brpd_time
+    orig_time = apd_time - bpd_time
+    
+    print 'scalar mulitplication:'
+    print 'ref =', ref_time
+    print 'ecp =', orig_time
+
+    testset = []
+    mpztestset = []
+    for x in range(25):
+        r = random.randint(0, _curve['n'] - 1)
+        testset.append(r)
+    
+    tval = []
+    brpd_time = time.time()
+    for x in testset:
+        for y in range(100):
+            X = fast_multiply(G,x)
+    arpd_time = time.time()
+    
+    tval = []
+    bpd_time = time.time()
+    for x in testset:
+        for y in range(100):
+            X = Gpt * x
+    apd_time = time.time()
+    
+    tval = []
+    bgpdb_time = time.time()
+    for x in testset:
+        for y in range(100):
+            X = GenG * x
+    agpdb_time = time.time()
+    
+    ref_time = arpd_time - brpd_time
+    orig_time = apd_time - bpd_time
+    gnew_time = agpdb_time - bgpdb_time
+
+    print 'generation:'
+    print 'ref =', ref_time
+    print 'ecp =', orig_time
+    print 'gen =', gnew_time
+    
