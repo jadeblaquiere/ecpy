8a6d6d5476c69f3a733f06b824018b65bb865a7e
jadeblaquiere <jadeblaquiere@yahoo.com>
2016-05-03 07:52:06 -0400

    adding setup.py metadata

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup
+from ecpy import __version__
 
-setup(name='ECPY: Pure python elliptic curve math', packages=['ecpy'])
+setup(name='ECPY: Pure python elliptic curve math', 
+      version=str(__version__),
+      author='Joseph deBlaquiere',
+      author_email='jadeblaquiere@yahoo.com',
+      packages=['ecpy'])
