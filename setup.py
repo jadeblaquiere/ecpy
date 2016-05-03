32e3b9f600d0e740e24616356e4ddc0dbe853078
jadeblaquiere <jadeblaquiere@yahoo.com>
2016-05-03 08:00:15 -0400

    adding description to metadata

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from ecpy import __version__
 
 setup(name='ECpy',
-      description='Python elliptic curve math'
+      description='Python elliptic curve math',
       version=str(__version__),
       author='Joseph deBlaquiere',
       author_email='jadeblaquiere@yahoo.com',
